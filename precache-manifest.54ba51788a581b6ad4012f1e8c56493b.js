b76e7dfd916402eb8da67f48019439cbe78548ed
Venipa <admin@venipa.net>
2019-07-12 23:55:26 +0200

    Updates

---
+++ precache-manifest.54ba51788a581b6ad4012f1e8c56493b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b546f94050717d7eab98ca064d208ed",
+    "url": "/neko-splash/index.html"
+  },
+  {
+    "revision": "d3b41073d43b0800cbd7",
+    "url": "/neko-splash/static/css/main.fc4d8b52.chunk.css"
+  },
+  {
+    "revision": "90c45c366834d95d4183",
+    "url": "/neko-splash/static/js/2.a667ff77.chunk.js"
+  },
+  {
+    "revision": "d6c92399766d84267a33",
+    "url": "/neko-splash/static/js/3.3fe23f4f.chunk.js"
+  },
+  {
+    "revision": "d3b41073d43b0800cbd7",
+    "url": "/neko-splash/static/js/main.9bdc6605.chunk.js"
+  },
+  {
+    "revision": "89f44e429e4616cc7b89",
+    "url": "/neko-splash/static/js/runtime~main.6cb16372.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/neko-splash/precache-manifest.452fca9a56adea786dd070ad60b26177.js"
+  "/neko-splash/precache-manifest.54ba51788a581b6ad4012f1e8c56493b.js"
 );
 
 self.addEventListener('message', (event) => {
